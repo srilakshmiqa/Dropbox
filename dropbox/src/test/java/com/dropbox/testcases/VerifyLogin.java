4704e324cac7abc83f49b3feab57e21f1227108c
srilakshmik <srilakshmik@IQIND026.corp.inquest.com>
2018-09-04 17:44:54 +0530

    BasePage commit

--- dropbox/src/test/java/com/dropbox/testcases/VerifyLogin.java
+++ dropbox/src/test/java/com/dropbox/testcases/VerifyLogin.java
@@ -1,10 +1,22 @@
 package com.dropbox.testcases;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.dropbox.pageobjects.BasePage;
+
 public class VerifyLogin extends TestBase{
+	BasePage basePage;
+	@BeforeClass
+	public void objectsIntialization() {
+		basePage = PageFactory.initElements(driver, BasePage.class);
+	}
+	
   @Test
-  public void f() {
+  public void verifyLogin() {
 	  System.out.println("test");
+	  basePage.userLogin("test", "test");
   }
 }
